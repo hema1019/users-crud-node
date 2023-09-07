56f7151d5f4168e9fa224184bff9d69d7d11e8c6
Hugo Moya <133818289+hema1019@users.noreply.github.com>
2023-09-07 14:21:26 -0600

    cors enabled

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import db from "./utils/database.js";
 import User from "./models/users.model.js";
 import "dotenv/config";
@@ -24,6 +25,7 @@ db.sync() // si no existe la tabla -> la crea / si ya existe hace nada
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // health check
 app.get("/", (req, res) => {
